12c6a795f4451c166d81a5842861d8a7d65f0121
Paul Gasbarra <paulgasbarra@gmail.com>
2025-04-07 17:54:50 -0400

    add support mentor page

---
+++ src/pages/support-mentors.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+const About: React.FC = () => {
+  return <Layout>Support Mentors</Layout>;
+};
+
+export default About;
