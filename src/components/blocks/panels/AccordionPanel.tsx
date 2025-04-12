d10f496445a0463e8c43b14099e8b14afd43595f
Paul Gasbarra <paulgasbarra@gmail.com>
2025-04-12 06:43:02 -0400

    fix: break accordian panels out

---
+++ src/components/blocks/panels/AccordionPanel.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import * as styles from './AccordionPanel.module.css';
+
+interface AccordionItemProps {
+  title: string;
+  textItems: string[];
+  expanded?: boolean;
+}
+
+export const AccordionItem: React.FC<AccordionItemProps> = ({
+  title,
+  textItems,
+  expanded = false,
+}) => {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+  return (
+    <div className={styles.accordionItem}>
+      <h3 className={styles.accordionItemTitle}>
+        {title}
+        <span
+          className={styles.accordionItemArrow}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          {isExpanded ? '-' : '+'}
+        </span>
+      </h3>
+      <p
+        className={
+          styles.accordionItemText + ' ' + (isExpanded ? styles.expanded : '')
+        }
+      >
+        <ul>{textItems && textItems.map((item) => <li>{item}</li>)}</ul>
+      </p>
+    </div>
+  );
+};
+
+export const AccordionPanel: React.FC<AccordianPanelProps> = ({
+  accordionItems,
+}) => {
+  return (
+    <div className={styles.accordionItems}>
+      {accordionItems.map((item) => (
+        <AccordionItem
+          key={item.title}
+          title={item.title}
+          textItems={item.textItems}
+        />
+      ))}
+    </div>
+  );
+};
--- src/components/blocks/two-panel/AccordionBlock.tsx
+++ src/components/blocks/two-panel/AccordionBlock.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import * as styles from './AccordionBlock.module.css';
 import { ImagePanel } from '../panels/ImagePanel';
+import { AccordionPanel } from '../panels/AccordionPanel';
 
 interface AccordionBlockProps {
   title: string;
@@ -12,34 +13,6 @@ interface AccordionBlockProps {
   textOrientation?: 'left' | 'right' | 'center'; // Optional text orientation with default
   theme?: 'dark' | 'light';
 }
-
-export const AccordionItem: React.FC<AccordionItemProps> = ({
-  title,
-  textItems,
-  expanded = false,
-}) => {
-  const [isExpanded, setIsExpanded] = useState(expanded);
-  return (
-    <div className={styles.accordionItem}>
-      <h3 className={styles.accordionItemTitle}>
-        {title}
-        <span
-          className={styles.accordionItemArrow}
-          onClick={() => setIsExpanded(!isExpanded)}
-        >
-          {isExpanded ? '-' : '+'}
-        </span>
-      </h3>
-      <p
-        className={
-          styles.accordionItemText + ' ' + (isExpanded ? styles.expanded : '')
-        }
-      >
-        <ul>{textItems && textItems.map((item) => <li>{item}</li>)}</ul>
-      </p>
-    </div>
-  );
-};
 
 export const AccordionBlock: React.FC<FeatureBlockProps> = ({
   title,
@@ -53,15 +26,7 @@ export const AccordionBlock: React.FC<FeatureBlockProps> = ({
       <h2 className={styles.accordionTitle}>{title}</h2>
       <div className={styles.accordionContainer}>
         <ImagePanel imageSrc={imageSrc} imageAlt={imageAlt} />
-        <div className={styles.accordionItems}>
-          {accordionItems.map((item) => (
-            <AccordionItem
-              key={item.title}
-              title={item.title}
-              textItems={item.textItems}
-            />
-          ))}
-        </div>
+        <AccordionPanel accordionItems={accordionItems} />
       </div>
     </div>
   );
