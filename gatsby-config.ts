01b9a78b469d022cb41b1c221fb7b6fd2f0d4b9e
Paul Gasbarra <paulgasbarra@gmail.com>
2025-04-28 14:40:06 -0400

    adding some seo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,31 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
-  // don't need to define it here (just if you need to change the options)
-  plugins: [`gatsby-plugin-pnpm`],
+  siteMetadata: {
+    title: `Stacks+Joules`,
+    description: `Stacks+Joules is a nonprofit project-based learning program`,
+    siteUrl: `https://stacksandjoules.org`, // Required for sitemap
+    author: `Stacks+Joules`,
+  },
+  plugins: [
+    `gatsby-plugin-pnpm`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/sitemap`,
+        createLinkInHead: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: `https://stacksandjoules.org`,
+        sitemap: `https://stacksandjoules.org/sitemap/sitemap-index.xml`,
+        policy: [{ userAgent: `*`, allow: `/` }],
+      },
+    },
+  ],
   jsxRuntime: `automatic`,
 };
 
